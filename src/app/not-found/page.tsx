61b3c5245374af7e8a4b257f0d64cbba5ca2aa57
Your Name <you@example.com>
2025-11-19 23:45:31 +0500

    фиксирует пул, забыл оттолкнуться от main

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import styles from './not-found.module.css'
+
+export default function NotFoundPage() {
+    return (
+        <div className={styles['not - found']}>
+            <h1>404 Привет</h1>
+            <p>Страница не найдена</p>
+            <Link href="/">Вернуться на главную</Link>
+        </div>
+    )
+}
