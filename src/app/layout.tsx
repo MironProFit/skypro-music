aefb234eb77e3dff88e8029cc9998ee2fe5bae86
Your Name <you@example.com>
2026-02-07 23:52:11 +0500

    интегрирует тост уведомления

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import ReduxProvider from 'src/store/ReduxProvider'
 import { HydrationWrapper } from '@components/HydrationWrapper/HydrationWrapper'
 import InitialDataLoader from '@components/InitialDataLoader/InitialDataLoader'
+import { ToastProvider } from '@components/ToastProvider/ToastProvider'
 
 const montserrat = Montserrat({
   variable: '--font-montserrat',
@@ -24,8 +25,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={montserrat.variable}>
         <ReduxProvider>
-          <InitialDataLoader />
-          <HydrationWrapper>{children}</HydrationWrapper>
+          <ToastProvider>
+            <InitialDataLoader />
+            <HydrationWrapper>{children}</HydrationWrapper>
+          </ToastProvider>
         </ReduxProvider>
       </body>
     </html>
